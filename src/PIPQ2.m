e8b16bd2de0d4490f55938dd64c48b534cf6f986
Tobias Keller <tobias.keller@glasgow.ac.uk>
2020-11-11 08:40:52 +0000

    fixed projection from IP to Q2 to avoid over/undershoot of quadratic functions

--- src/PIPQ2.m
+++ src/PIPQ2.m
@@ -12,16 +12,18 @@
 
 function  [Q2field]  =  PIPQ2(IPfield,FE)
 
-n        =  length(IPfield(1,:));
-Q2field  =  zeros(FE.NQ2,n);
-N        =  FE.NiQ2;
+Q2field = PQ1Q2(PIPQ1(IPfield,FE),FE);
 
-for k=1:n
-    
-    TMP           =  IPfield(:,k);
-    TMP           =  (TMP(FE.El2IP)*N') ./ (ones(size(FE.El2IP))*N');
-    Q2field(:,k)  =  accumarray(FE.El2Q2(:),TMP(:)) ./ accumarray(FE.El2Q2(:),ones(size(TMP(:))));
-    
-end
+% n        =  length(IPfield(1,:));
+% Q2field  =  zeros(FE.NQ2,n);
+% N        =  FE.NiQ2;
+% 
+% for k=1:n
+%     
+%     TMP           =  IPfield(:,k);
+%     TMP           =  (TMP(FE.El2IP)*N') ./ (ones(size(FE.El2IP))*N');
+%     Q2field(:,k)  =  accumarray(FE.El2Q2(:),TMP(:)) ./ accumarray(FE.El2Q2(:),ones(size(TMP(:))));
+%     
+% end
 
 end
